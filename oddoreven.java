72cc539e1fe5200c25996c1e63e747df560211d1
chrisance2002 <chrisance2002@gmail>
2024-02-07 10:28:44 +0530

    new

--- oddoreven.java
+++ oddoreven.java
@@ -1,19 +1,15 @@
-import java.util.Scanner;
 public class oddoreven
 {
     public static void main (String [] args)
     {
-        Scanner s1= new Scanner(System.in);
-        System.out.print("Number: ");
-        int a= s1.nextInt();
-        System.out.println("Number is: "+a);
+        int a=22;
         if(a%2==0)
         {
-            System.out.println("The number " +a+ " is even");
+            System.out.println("The number is even");
         }
         else
         {
-            System.out.println("The number " +a+ " is odd");
+            System.out.println("The number is odd");
         }
     }
 }
